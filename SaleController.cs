de236694e2219f8525e12332909673ca21b8a638
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-11-09 18:24:25 -0600

    Add files via upload

---
+++ SaleController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ExampleAPI.Controllers
+{
+	public class SaleController
+	{
+		public SaleController()
+		{
+		}
+	}
+}
+
---
+++ UserController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ExampleAPI.Controllers
+{
+	public class UserController
+	{
+		public UserController()
+		{
+		}
+	}
+}
+
