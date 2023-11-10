117a340e5706468e5d671b35a6c621bdb805836a
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-11-10 13:14:16 -0600

    Create Payment.cs

---
+++ Model/Payment.cs
@@ -0,0 +1,24 @@
+using System;
+namespace ExampleAPI.Models
+/*
+ not sure what field we want expiration date to be
+*/
+{
+    public class Payment
+	{
+		public int paymentId { get; set; }
+		public int cardNumber { get; set; }
+		public string expirationDate { get; set;}
+		public int cvvNumber { get; set; }
+		public Address billingAddress { get; set; }
+
+        public Payment(int paymentId, int cardNumber, string expirationDate, int cvvNumber, Address billingAddress)
+        {
+            this.paymentId = paymentId;
+            this.cardNumber = cardNumber;
+            this.expirationDate = expirationDate;
+            this.cvvNumber = cvvNumber;
+            this.billingAddress = billingAddress;
+        }
+    }
+}
