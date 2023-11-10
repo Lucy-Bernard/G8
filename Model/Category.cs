3beadea53d4c5629d50583122ad4abe6fac5ff31
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-11-10 13:13:16 -0600

    Create Category.cs

---
+++ Model/Category.cs
@@ -0,0 +1,23 @@
+using System;
+namespace ExampleAPI.Models
+/*- categoryName:String
+- categoryItems:List<Product>
+ */
+{
+    public class Category
+	{
+        public int categoryId { get; set; }
+        public string categoryName { get; set; }
+		public List<Product> categoryItems { get; set; }
+
+        public Category(int categoryId, string categoryName, List<Product> categoryItems)
+        {
+            this.categoryId = categoryId;
+            this.categoryName = categoryName;
+            this.categoryItems = categoryItems;
+        }
+    }
+
+
+
+}
