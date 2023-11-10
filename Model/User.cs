1be74e2135f847a35331d45ed71ac7f77dfe42b5
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-11-10 13:15:04 -0600

    Create User.cs

---
+++ Model/User.cs
@@ -0,0 +1,39 @@
+using System;
+namespace ExampleAPI.Models
+/*- firstName:String
+- lastName:String 
+- email:String
+- password:String
+- phoneNumber:int
+- shippingAddress Address
+- paymentInfo:Payment
+*/
+
+
+{
+    public class User
+	{
+        public int userId { get; set; }
+        public string firstname { get; set; }
+		public string lastname { get; set; }
+		public string email { get; set; }
+		public string password { get; set; }
+		public int phoneNumber { get; set; }
+		public Address shippingAddress { get; set; }
+		public Payment paymentinfo { get; set; }
+
+        public User(int userId, string firstname, string lastname, string email, string password,
+            int phoneNumber, Address shippingAddress, Payment paymentinfo)
+        {
+            this.userId = userId;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.email = email;
+            this.password = password;
+            this.phoneNumber = phoneNumber;
+            this.shippingAddress = shippingAddress;
+            this.paymentinfo = paymentinfo;
+        }
+    }
+	}
+
