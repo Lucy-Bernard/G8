ad85bca5e00b5bfc34338e89d81be5e2bcf44d7c
isaiahstone21 <isstone21@hotmail.com>
2023-11-11 15:53:19 -0600

    Created navbar and created userprofile page

---
+++ FrontEnd/src/app/userprofile/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./page.module.css";
+
+const UserProfile = () => {
+  return (
+      <div className={styles.test}>
+        <h1>UserProfile</h1>
+      </div>
+  );
+};
+
+export default UserProfile;
