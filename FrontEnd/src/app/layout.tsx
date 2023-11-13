83d74b29fc3458e5ad79d943a5a3e1fc22c11b14
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-11-13 12:33:44 -0600

    nate trying to push

--- FrontEnd/src/app/layout.tsx
+++ FrontEnd/src/app/layout.tsx
@@ -2,6 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavigationBar, { PageLink } from "@/components/NavigationBar/NavigationBar";
+import Header from "@/components/Header/Header";
+import Banner from "@/components/Banner/Banner"; // Import the Banner component
+
 
 const inter = Inter({ subsets: ["latin"] });
 const links: Array<PageLink> = [
@@ -9,7 +12,7 @@ const links: Array<PageLink> = [
     link_text: "Products",
     link_url: "products"
   }
-]
+];
 
 export const metadata: Metadata = {
   title: "Frontend Setup",
@@ -20,9 +23,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Header />
+         
         <NavigationBar links={links} />
+        <Banner /> 
         {children}
       </body>
     </html>
   )
 }
+
