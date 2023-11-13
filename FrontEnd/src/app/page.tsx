74baf6f86f9ed1c0ace193844e96dc5aab4b2b05
Isaiah Stone <isstone21@hotmail.com>
2023-11-13 13:02:55 -0600

    Added comment to explain how routing works for home page

--- FrontEnd/src/app/page.tsx
+++ FrontEnd/src/app/page.tsx
@@ -1,6 +1,11 @@
 import ProductCard from "@/components/ProductCard/ProductCard";
 import styles from "./page.module.css"
 
+/**
+ * This is the website home page and default routing location.
+ * 
+ */
+
 export default function Home() {
   return (
     <main className={styles.main}>
