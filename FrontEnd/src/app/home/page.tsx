699523702617e5456ed24a6a0366b4e7b8066dc5
joeyngg <111897766+joeyngg@users.noreply.github.com>
2023-11-16 11:19:50 -0600

    modify the path

--- FrontEnd/src/app/home/page.tsx
+++ FrontEnd/src/app/home/page.tsx
@@ -1,21 +1,18 @@
 import ProductCard from "@/components/ProductCard/ProductCard";
 import styles from "./page.module.css"
-import Header from "@/components/Header/Header";
 import Banner from "@/components/Banner/Banner";
-import NavigationBar from "@/components/NavigationBar/NavigationBar";
 import ProductSection from "@/components/ProductSection/ProductSection";
+import Products from "../products/page";
 
 export default function Home() {
+  
   return (
-
     <main>
-     
       <Banner />
       <div className={styles.main}>
         <h1>Home</h1>
-        <ProductSection title={""} products={[]}/>
+        <ProductSection title={"Tops"} products={topsProducts}/>
       </div>
-      
     </main>
   )
 }
