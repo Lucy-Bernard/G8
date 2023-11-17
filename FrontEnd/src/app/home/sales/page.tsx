ab2dffaa5333224b470a95fb18a3167aed2041fc
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-11-17 13:23:01 -0600

    adding unit test and sale page

---
+++ FrontEnd/src/app/home/sales/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from "./page.module.css";
+
+const sales = () => {
+  return (
+    <div className={styles.test}>
+      <h1>sales</h1>
+      <Link href="/home/sales" passHref>
+         
+      </Link>
+    </div>
+  );
+};
+
+export default sales;
