135d2289426f695af9775ff2c5c246034db3c39c
isaiahstone21 <isstone21@hotmail.com>
2023-11-18 20:29:27 -0600

    Created routes for Tops, bottoms, etc. Also created Cart model.

---
+++ FrontEnd/src/app/home/outerwear/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./page.module.css";
+
+const Outerwear = () => {
+  return (
+      <div className={styles.test}>
+        <h1>Outerwear</h1>
+      </div>
+  );
+};
+
+export default Outerwear;
---
+++ FrontEnd/src/app/home/tops/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./page.module.css";
+
+const Tops = () => {
+  return (
+      <div className={styles.test}>
+        <h1>Tops</h1>
+      </div>
+  );
+};
+
+export default Tops;
