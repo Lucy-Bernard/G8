2b465a06758c1766b04c5d2677f37b32e2eea980
joeyngg <111897766+joeyngg@users.noreply.github.com>
2023-12-01 14:15:31 -0600

    product details

--- FrontEnd/src/app/home/productdetails/page.tsx
+++ FrontEnd/src/app/home/productdetails/page.tsx
@@ -1,17 +1,63 @@
-"use client"
+"use client";
 
-import React, { useState } from "react";
-import ProductDetails from "@/components/ProductDetails/ProductDetails";
+import React, { useEffect, useState } from "react";
+import styles from "../page.module.css";
 
-const ProductDetailsPage = () => {
-  const [selectedProductId, setSelectedProductId] = useState<string | null>(null);
+type ProductDetailsData = {
+  productId: number;
+  productName: string;
+  unitPrice: number;
+  manufacturer: string;
+  description: string;
+  rating: number;
+  sku: string;
+  imageLink: string;
+};
 
+const ProductDetails = (props: ProductDetailsData) => {
+  const [productDetails, setProductDetails] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    fetch(`http://localhost:5165/api/product/${props.productId}`, {
+      method: "GET",
+      headers: myHeaders,
+      redirect: "follow",
+    })
+      .then((response) => response.json())
+      .then((result) => setProductDetails(result))
+      .catch((error) => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, [props.productId]);
 
   return (
-    <div>
-      <ProductDetails productId={1} unitPrice={0} imageLink={""} productName={""} manufacturer={""} description={""} rating={0} sku={""} />
-    </div>
+    <main>
+      <h1>Product Details</h1>
+      {productDetails ? (
+        <div>
+            <div className={styles.product_productId}>
+                <p>ProductId: {productDetails.productId} </p></div>
+            <img src={productDetails?.imageLink} alt={productDetails?.productName} />
+            <h2>{productDetails.productName}</h2>
+            <p>Price: ${productDetails.unitPrice} </p>
+            <div className={styles.product_manufacturer}>
+                <p>Manufacturer: {productDetails.manufacturer} </p></div>
+            <div className={styles.product_description}>
+                <p>Description: {productDetails.description} </p></div>
+            <div className={styles.product_rating}>
+                <p>Rating: {productDetails.rating} </p></div>
+            <div className={styles.product_sku}>
+                <p>SKU: {productDetails.sku} </p></div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </main>
   );
 };
 
-export default ProductDetailsPage;
+export default ProductDetails;
