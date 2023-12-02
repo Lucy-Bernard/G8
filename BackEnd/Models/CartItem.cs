e45d7f77d62ff07343b0c9ff0035d3cc8bbbeef2
isaiahstone21 <isstone21@hotmail.com>
2023-12-02 17:06:54 -0600

    cart works with all crud operations

--- BackEnd/Models/CartItem.cs
+++ BackEnd/Models/CartItem.cs
@@ -6,11 +6,11 @@ namespace ExampleAPI.Models
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
         public string? ProductName { get; set; }
-        public double UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
         public string? Manufacturer { get; set; }
         public string? Description { get; set; }
-        public double Rating { get; set; }
-        public int Sku { get; set; }
+        public decimal Rating { get; set; }
+        public string? Sku { get; set; }
         public string? ImageLink { get; set; }
         public int Quantity { get; set; }
         public int UserId { get; set; }
@@ -19,5 +19,22 @@ namespace ExampleAPI.Models
             this.UserId = UserId;
             this.ProductId = ProductId;
         }
+
+        public CartItem(int CartItemId, int ProductId, int Quantity, string ProductName, decimal UnitPrice, string Manufacturer, string Description, decimal Rating, string Sku, string ImageLink)
+        {
+            this.CartItemId = CartItemId;
+            this.ProductId = ProductId;
+            this.Quantity = Quantity;
+            this.ProductName = ProductName;
+            this.UnitPrice = UnitPrice;
+            this.Manufacturer = Manufacturer;
+            this.Description = Description;
+            this.Rating = Rating;
+            this.Sku = Sku;
+            this.ImageLink = ImageLink;
+        }
+
+        public CartItem() {}
+
     }
 }
