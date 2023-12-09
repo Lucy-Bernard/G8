641696fb5d9a82f62a93253d35e713fad4fcbc88
nmatthews5 <112026811+nmatthews5@users.noreply.github.com>
2023-12-09 13:55:37 -0600

    documentation

--- FrontEnd/src/tests/ProductSection.test.js
+++ FrontEnd/src/tests/ProductSection.test.js
@@ -3,15 +3,20 @@ import { render } from "@testing-library/react";
 import React from "react";
 import ProductSection from "../components/ProductSection/ProductSection";
 
+  /**
+   * Test case: Renders the component with sample products and matches the snapshot.
+   */
 describe('ProductSection component', () => {
   it('renders as expected', () => {
     const products = [
       { productId: 1, productName: 'Product 1', unitPrice: 10.99, imageLink: 'product1.jpg' },
       { productId: 2, productName: 'Product 2', unitPrice: 19.99, imageLink: 'product2.jpg' },
-      // Add more sample products as needed
+     
     ];
 
+    // Render the ProductSection component with sample products
     const { container } = render(<ProductSection title="Featured Products" products={products} />);
+    // Assert that the rendered component matches the snapshot
     expect(container).toMatchSnapshot();
   });
 });
--- FrontEnd/src/tests/Search.test.js
+++ FrontEnd/src/tests/Search.test.js
@@ -2,6 +2,7 @@ import "@testing-library/jest-dom"
 import { render } from "@testing-library/react"
 import Search from '../components/Search/Search'
 
+// Mocking the 'next/navigation' module for testing
 jest.mock('next/navigation', () => ({
     useRouter() {
         return {
@@ -10,9 +11,15 @@ jest.mock('next/navigation', () => ({
     }
 }))
 
+//Mocking the global fetch function
 fetch = jest.fn(() => Promise.resolve());
 
 it('component renders as expected', async () => {
+        //Render the Search component
     const { container } = render(<Search />)
+        // Assert that the rendered component matches the snapshot
     expect(container).toMatchSnapshot()
 })
+
+
+
